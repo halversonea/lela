3f1c4000b89628d21825ac7355b4eb204494dff6
Eric Halverson <ehalverson@gmail.com>
2023-02-19 09:10:34 -0500

    saving to share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import requests
-import pickle
 import json
+import requests
 
 from auth import *
 
@@ -16,17 +15,8 @@ body = {
   "num_records": 10,
 }
 
-# print(f"url = {URL}\n\nheaders = {HEADERS}\n\ndata = {body}")
-
 resp = requests.request(
     "POST", url=URL, headers=HEADERS, json=body,
 )
 
-#cache the response to keep working with data and not pay for API yet
-with open("cache/prop_resp_intl.txt", "wb") as f:
-    pickle.dump(resp,f)
-
-with open("cache/prop_resp_intl.txt", "rb") as f:
-    f_resp = pickle.load(f)
-
-print(f_resp.json())
+print(resp.json())
